1df80f6c6d984de4d4681eb8552bfb330dba505a
Daria Sokolova <65014485+PryanyyMed@users.noreply.github.com>
2020-11-16 18:26:21 +0300

    Update test_mypets.py

--- 25md/test_mypets.py
+++ 25md/test_mypets.py
@@ -1,14 +1,15 @@
+# python3 -m pytest -s -v --driver Chrome --driver-path /home/dasha/Documents/driver/chromedriver  test_mypets.py
+
 import pytest
 from selenium import webdriver
 import uu
 import os
 import uuid
-import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-# зайти на главную страницу со всеми петами
+# зайти на главную страницу со всеми петами, всех пользователей
 
 @pytest.mark.usefixtures("testing")
 def test_show_my_pets():
@@ -21,12 +22,10 @@ def test_show_my_pets():
     pytest.driver.find_element_by_id('pass').send_keys('123456Pp')
     # Нажимаем на кнопку входа в аккаунт
     pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
-    time.sleep(0)
 
     # Переход к моим петам
     pytest.driver.find_element_by_css_selector('button[type="button"]').click()
     pytest.driver.find_element_by_class_name("nav-link").click()
-    time.sleep(0)
 
     # Проверяем, что мы оказались на странице пользователя с его петами
     assert pytest.driver.find_element_by_tag_name('h2').text == "Dan"
@@ -34,24 +33,36 @@ def test_show_my_pets():
     # Явное ожидание появление элементов таблицы на странице
     wait = WebDriverWait(pytest.driver, 5).until(EC.presence_of_all_elements_located(('id', 'all_my_pets')))
 
-    # посчитать кол-во петов на странице
-    assert len(pytest.driver.find_elements_by_xpath('//*[@id="all_my_pets"]/table/tbody/tr')) == 6
+    # посчитать кол-во петов на странице, проверка, что все они есть на странице пользователя
+    pets = pytest.driver.find_elements_by_xpath('//*[@id="all_my_pets"]/table/tbody/tr')
+    assert len(pets) == 2
 
     # проверка, что у каждой карточки есть картинка, имя и описание
-    images = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
-    names = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
-    descriptions = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+    images = pytest.driver.find_element_by_xpath('//*[@id="all_my_pets"]/table/tbody/tr/th/img')
 
-    for i in range(len(names)):
-        # хотябы у половины петов есть картинка
-        assert images[i].get_attribute('src') >= 2
-        # у всех есть имя
-        assert names[i].text != ''
-        # у всех есть описание
-        assert descriptions[i].text != ''
-        assert ', ' in descriptions[i]
-        parts = descriptions[i].text.split(", ")
-        assert len(parts[0]) > 0
-        assert len(parts[1]) > 0
-        # у всех разные имена
-        assert names.text != names[3].text
+    for i in range(len(pets)):
+        # проверка наличия картинок у всех петов
+        print(images.is_displayed())
+    # у всех есть имя, описание и возраст
+    assert pytest.driver.find_elements_by_css_selector('#all_my_pets tr')[1].text == "Crazy Cat crazy 1\n×"
+    assert pytest.driver.find_elements_by_css_selector('#all_my_pets tr')[2].text == "Test testing cat 21\n×"
+
+    """ # таже проверка, но через xpath
+    assert pytest.driver.find_element_by_xpath(
+            '//*[@id="all_my_pets"]/table/tbody/tr/td').text == "Crazy Cat"
+    assert pytest.driver.find_element_by_xpath(
+            '//*[@id="all_my_pets"]/table/tbody/tr[2]/td').text == "Test"
+    # у всех есть описание
+    assert pytest.driver.find_element_by_xpath(
+        '//*[@id="all_my_pets"]/table/tbody/tr/td[2]').text == "crazy"
+    assert pytest.driver.find_element_by_xpath(
+        '//*[@id="all_my_pets"]/table/tbody/tr[2]/td[2]').text == "testing cat"
+    # у всех есть возраст
+    assert pytest.driver.find_element_by_xpath(
+        '//*[@id="all_my_pets"]/table/tbody/tr/td[3]').text == "1"
+    assert pytest.driver.find_element_by_xpath(
+        '//*[@id="all_my_pets"]/table/tbody/tr[2]/td[3]').text == "21"
+    # у всех разные имена
+    assert pytest.driver.find_element_by_xpath(
+        '//*[@id="all_my_pets"]/table/tbody/tr/td').text != pytest.driver.find_element_by_xpath(
+        '//*[@id="all_my_pets"]/table/tbody/tr[2]/td').text"""
